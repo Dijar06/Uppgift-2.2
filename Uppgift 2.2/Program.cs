db8c698c3093deec75241cac46a165d2ab03fcbf
Dijar06 <dize06001@utb.vaxjo.se>
2023-09-15 12:33:17 +0200

    Add project files.

---
+++ Uppgift 2.2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Uppgift2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur långt hoppade Elin i längdhopp?");
+            float x = float.Parse(Console.ReadLine());
+            Console.WriteLine("Hur långt hoppade Alma i längdhopp?");
+            float y = float.Parse(Console.ReadLine());
+            float z = x - y;
+            Console.WriteLine("Elin hoppade " + z + " meter längre än Alma");
+        }
+    }
+}
